34dbcc3b85c34ad32d724be0851cf9d76a515fb9
Sergio Melero <sergiomele97@gmail.com>
2024-05-08 18:18:38 +0200

    Agregar archivos de proyecto.

---
+++ LargestSumContiSA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntrTelefonica
+{
+    internal class LargestSumContiSA
+    {
+        /* Given an array arr[] of size N. 
+         * The task is to find the sum of the contiguous subarray within a arr[] with the largest sum. 
+        */
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntrTelefonica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solo para imprimir resultados:
+
+            // Console.WriteLine(LargestSumConSubarray());
+            // FindDuoValue();
+            Console.WriteLine(IsBalanced());
+        }
+
+
+        public static bool IsBalanced()
+        {
+            string str = "{[]}()[][][][[{[]}]]"; // Correcto
+            string listadoApertura = "";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                switch (str[i])
+                {
+                    case '{':
+                        listadoApertura += '{';
+                        break;
+                    case '}':
+                        if (listadoApertura.Length >= 1 && listadoApertura[listadoApertura.Length - 1] == '{')
+                        {
+                            listadoApertura = listadoApertura.Remove(listadoApertura.Length - 1, 1);
+                        }
+                        else { return false; }
+                        break;
+                    case '(':
+                        listadoApertura += '(';
+                        break;
+                    case ')':
+                        if (listadoApertura.Length >= 1 && listadoApertura[listadoApertura.Length - 1] == '(')
+                        {
+                            listadoApertura = listadoApertura.Remove(listadoApertura.Length - 1, 1);
+                        }
+                        else { return false; }
+                        break;
+                    case '[':
+                        listadoApertura += '[';
+                        break;
+                    case ']':
+                        if (listadoApertura.Length >= 1 && listadoApertura[listadoApertura.Length - 1] == '[')
+                        {
+                            listadoApertura = listadoApertura.Remove(listadoApertura.Length - 1, 1);
+                        }
+                        else { return false; }
+                        break;
+                }
+            }
+            
+
+            if (listadoApertura.Length == 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+        public static void FindDuoValue()
+        {
+            int[] arr = new int[] { -2, -3, 4, -1, -2, 1, 5, -3 };
+            int s = 4;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i; j < arr.Length; j++)    // Complejidad O(n^2) (cuadrática)
+                {
+                    if (arr[i] + arr[j] == s)
+                    {
+                        Console.WriteLine($"Valor {s} encontrado sumando las posiciones: " +
+                                          $"{i} (valor: {arr[i]}) y {j} (valor: {arr[j]}).");
+                    }
+                }
+            }
+        }
+
+        public static string LargestSumConSubarray()  // Algoritmo de Kadane’s: Suponemos que hay valores positivos en el array.
+        {
+            int[] arr = new int[]{ -2, -3, 4, -1, -2, 1, 5, -3 };
+
+            int max_ending_here = 0;
+            int max_so_far = 0;
+            int newi = 0;
+            int imax = 0;
+            int jmax = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                max_ending_here = max_ending_here + arr[i];     // 1
+
+                if (max_ending_here > max_so_far)               // 2 Actualizar MaxSoFar
+                {
+                    max_so_far = max_ending_here;
+                    jmax = i;
+                    imax = newi;
+                }
+
+                if (max_ending_here < 0)                        // 3 Reseteo si negativo
+                {
+                    max_ending_here = 0;
+                    newi = i + 1;   // Empezamos en el siguiente
+                }
+
+            }
+
+            return ($"El subarray con mayor valor empieza en {imax} y acaba en {jmax}. Con una puntuación de {max_so_far}");
+        }
+
+    }
+}
